d258d508b985dc2426bdf1478399e9c0f1ff2c54
Michael Brandt <michaelbrandt5@gmail.com>
2020-09-04 16:14:20 -0600

    fix \ syntax

--- bamboo/deploy/aws.sh
+++ bamboo/deploy/aws.sh
@@ -6,10 +6,10 @@ set -o pipefail
 # enable interruption signal handling
 trap - INT TERM
 docker run --rm \
-	-t \$(tty &>/dev/null && echo "-i") \
-	-e "AWS_ACCESS_KEY_ID=\${AWS_ACCESS_KEY_ID}" \
-	-e "AWS_SECRET_ACCESS_KEY=\${AWS_SECRET_ACCESS_KEY}" \
-	-e "AWS_DEFAULT_REGION=\${AWS_DEFAULT_REGION}" \
-	-v "\$(pwd):/project" \
+	-t $(tty &>/dev/null && echo "-i") \
+	-e "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" \
+	-e "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" \
+	-e "AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}" \
+	-v "$(pwd):/project" \
 	maven.earthdata.nasa.gov/aws-cli \
-	"\$@"
+	"$@"
