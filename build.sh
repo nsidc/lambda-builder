c3c32d2a2a68b17626ca4ad1ebc584044c4ef81d
MARK SCHWAB <32745414+schwabm@users.noreply.github.com>
2020-12-16 14:59:44 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -26,14 +26,19 @@ LAST_REQ_HASH=`curl -k -u $bamboo_maven_user:$bamboo_maven_password -v "https://
 
 CURR_REQ_HASH=`/usr/bin/md5sum ${PROJECT_DIR}/requirements.txt`
 
+echo "LAST HASH $LAST_REQ_HASH"
+echo "CURR HASH $CURR_REQ_HASH"
+
 # Compare Current and Previous Requriements.txt file md5 checksum hashes, and to save time rezip if requirements have not changed
-if [[ ${LAST_REQ_HASH} == ${CURR_REQ_HASH} ]] 
+if [ "${LAST_REQ_HASH}" = "${CURR_REQ_HASH}" ]
 then
+  echo "IN IF STATEMENT"
   curl -k -u $bamboo_maven_user:$bamboo_maven_password -v "https://maven.earthdata.nasa.gov/repository/nsidc/${LAMBDA_NAME}.zip" -o ./${LAMBDA_NAME}_OLD.zip
   unzip ${LAMBDA_NAME}_OLD.zip -d OLD/
   cp -r ${PROJECT_DIR}/src/* ./OLD/
   zip -X -r ${LAMBDA_NAME}.zip ./OLD/* 
 else
+  echo "IN ELSE STATMENT"
   # make this script work no matter where it was called from
   BUILDER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
