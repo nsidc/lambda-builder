81efdc3729575bcdbda619584bcec8ae19d84bb4
MARK SCHWAB <32745414+schwabm@users.noreply.github.com>
2020-12-16 14:48:58 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -22,21 +22,39 @@ if [ -z "${LAMBDA_NAME}" ]; then
     LAMBDA_NAME='lambda'
 fi
 
-# make this script work no matter where it was called from
-BUILDER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+LAST_REQ_HASH=`curl -k -u $bamboo_maven_user:$bamboo_maven_password -v "https://maven.earthdata.nasa.gov/repository/nsidc/requirements-${LAMBDA_NAME}.txt.md5"`
 
-# build docker image
-DOCKER_IMAGE_TAG=lambda-builder
-docker build -t ${DOCKER_IMAGE_TAG} ${BUILDER_DIR}
+CURR_REQ_HASH=`md5 ${PROJECT_DIR}/requirements.txt`
 
-# run the build script on the docker container
-docker run \
+# Compare Current and Previous Requriements.txt file md5 checksum hashes, and to save time rezip if requirements have not changed
+if [[ ${LAST_REQ_HASH} == ${CURR_REQ_HASH} ]] 
+then
+  curl -k -u $bamboo_maven_user:$bamboo_maven_password -v "https://maven.earthdata.nasa.gov/repository/nsidc/${LAMBDA_NAME}.zip" -o ./${LAMBDA_NAME}_OLD.zip
+  unzip ${LAMBDA_NAME}_OLD.zip -d OLD/
+  cp -r ${PROJECT_DIR}/src/* ./OLD/
+  zip -X -r ${LAMBDA_NAME}.zip ./OLD/* 
+else
+  # make this script work no matter where it was called from
+  BUILDER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+  # build docker imagecurl -k -u $bamboo_maven_user:$bamboo_maven_password -v "https://maven.earthdata.nasa.gov/repository/nsidc/
+  DOCKER_IMAGE_TAG=lambda-builder
+  docker build -t ${DOCKER_IMAGE_TAG} ${BUILDER_DIR}
+
+  # run the build script on the docker container
+  docker run \
        -v /var/run/docker.sock:/var/run/docker.sock \
        -v ${PROJECT_DIR}:${PROJECT_DIR} \
        -e PROJECT_DIR="${PROJECT_DIR}" \
        ${DOCKER_IMAGE_TAG}
 
 # move the build artifact to current directory
-mv ${PROJECT_DIR}/lambda.zip ${LAMBDA_NAME}.zip
+  mv ${PROJECT_DIR}/lambda.zip ${LAMBDA_NAME}.zip
+fi
+
+echo $CURR_REQ_HASH > requirements-${LAMBDA_NAME}.txt.md5
+
+curl -k --upload-file ${LAMBDA_NAME}.zip -u $bamboo_maven_user:$bamboo_maven_password -v "https://maven.earthdata.nasa.gov/repository/nsidc/"
+curl -k --upload-file requirements-${LAMBDA_NAME}.txt.md5 -u $bamboo_maven_user:$bamboo_maven_password -v "https://maven.earthdata.nasa.gov/repository/nsidc/"
 
 echo "Created ${LAMBDA_NAME}.zip"
