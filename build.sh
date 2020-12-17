bf982d817d771b3b09ab21132dcc7f4946968b3a
Michael Brandt <michaelbrandt5@gmail.com>
2020-12-17 15:29:37 -0700

    restore build.sh from main branch

--- build.sh
+++ build.sh
@@ -17,56 +17,26 @@ if [[ "${PROJECT_DIR}" != /* ]] || [[ ! -d "${PROJECT_DIR}" ]]; then
     exit 1
 fi
 
-LAMBDA_NAME=$(basename $PROJECT_DIR)
-#LAMBDA_NAME=$2
+LAMBDA_NAME=$2
 if [ -z "${LAMBDA_NAME}" ]; then
     LAMBDA_NAME='lambda'
 fi
 
-curl -k -u $bamboo_maven_user:$bamboo_maven_password -v "https://maven.earthdata.nasa.gov/repository/nsidc/requirements-${LAMBDA_NAME}.txt.md5" -O
+# make this script work no matter where it was called from
+BUILDER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-LAST_REQ_HASH=`cat requirements-${LAMBDA_NAME}.txt.md5 | sed 's: ::g'`
-CURR_REQ_HASH=`/usr/bin/md5sum ${PROJECT_DIR}/requirements.txt| awk '{print $1}' | sed 's: ::g'`
+# build docker image
+DOCKER_IMAGE_TAG=lambda-builder
+docker build -t ${DOCKER_IMAGE_TAG} ${BUILDER_DIR}
 
-echo "LAST HASH -${LAST_REQ_HASH}-"
-echo "CURR HASH -${CURR_REQ_HASH}-"
-
-# Compare Current and Previous Requriements.txt file md5 checksum hashes, and to save time rezip if requirements have not changed
-if [ "${LAST_REQ_HASH}" = "${CURR_REQ_HASH}" ]
-then
-  echo "IN IF STATEMENT"
-  curl -k -u $bamboo_maven_user:$bamboo_maven_password -v "https://maven.earthdata.nasa.gov/repository/nsidc/${LAMBDA_NAME}.zip" -o ./${LAMBDA_NAME}_OLD.zip
-  unzip ${LAMBDA_NAME}_OLD.zip -d OLD/
-  cp -r ${PROJECT_DIR}/src/* ./OLD/
-  (cd OLD && zip -X -r ../lambda.zip ./*)
-  PWD=`pwd`
-  echo PWD $PWD
-  ls
-  echo LISTING OLD TOO
-  ls OLD
-else
-  echo "IN ELSE STATMENT"
-  # make this script work no matter where it was called from
-  BUILDER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-  # build docker imagecurl -k -u $bamboo_maven_user:$bamboo_maven_password -v "https://maven.earthdata.nasa.gov/repository/nsidc/
-  DOCKER_IMAGE_TAG=lambda-builder
-  docker build -t ${DOCKER_IMAGE_TAG} ${BUILDER_DIR}
-
-  # run the build script on the docker container
-  docker run \
+# run the build script on the docker container
+docker run \
        -v /var/run/docker.sock:/var/run/docker.sock \
        -v ${PROJECT_DIR}:${PROJECT_DIR} \
        -e PROJECT_DIR="${PROJECT_DIR}" \
        ${DOCKER_IMAGE_TAG}
 
 # move the build artifact to current directory
-  mv ${PROJECT_DIR}/lambda.zip lambda.zip
-fi
-
-echo $CURR_REQ_HASH > requirements-${LAMBDA_NAME}.txt.md5
-
-curl -k --upload-file lambda.zip -u $bamboo_maven_user:$bamboo_maven_password -v "https://maven.earthdata.nasa.gov/repository/nsidc/${LAMBDA_NAME}.zip"
-curl -k --upload-file requirements-${LAMBDA_NAME}.txt.md5 -u $bamboo_maven_user:$bamboo_maven_password -v "https://maven.earthdata.nasa.gov/repository/nsidc/"
+mv ${PROJECT_DIR}/lambda.zip ${LAMBDA_NAME}.zip
 
 echo "Created ${LAMBDA_NAME}.zip"
