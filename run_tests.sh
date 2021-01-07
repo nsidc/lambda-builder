013326a1502cf18d249892d50ebf21e83a8635b6
hsimonoko <73315841+hsimonoko@users.noreply.github.com>
2021-01-07 15:54:36 -0700

    Update run_tests.sh
    forgot to change to find .. | xargs

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "shellcheck"
-find . -type f -name "*.sh" -exec shellcheck {} \;
+find . -type f -name "*.sh" -print0 | xargs -0 shellcheck
 shellcheck_exit=$?
 
 # exit with failure if any of the above commands failed
